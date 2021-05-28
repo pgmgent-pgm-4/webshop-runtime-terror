ebd3888d65c44c50f639239b569752bce8f3117f
Jamie-Lee Hart <71268169+pgm-jamihart@users.noreply.github.com>
2021-05-29 00:55:40 +0200

    about page fully functional

--- app/server/src/public/scripts/about.js
+++ app/server/src/public/scripts/about.js
@@ -5,14 +5,23 @@
       this.registerEventListeners();
     },
     cacheElements () {
-      this.hamburgerButton = document.getElementById('hamburger-icon');
+      this.aboutSegmentedControl = document.querySelector('.about__segmented-control');
+      this.aboutSegmentedControlSelected = document.querySelector('.about__segmented-control__selected');
+      this.aboutSegmentedControlStory = document.querySelector('.about__segmented-control__story');
+      this.aboutSegmentedControlMission = document.querySelector('.about__segmented-control__mission');
+      this.aboutStory = document.querySelector('.about-story');
+      this.aboutMission = document.querySelector('.about-mission');
 
     },
     registerEventListeners () {
-      if (this.hamburgerButton !== null) {
-        this.hamburgerButton.addEventListener('click', (e) => {
-          this.navOverlay.classList.add('nav-overlay--open');
-          this.navOverlayBg.classList.add('nav-overlay__bg--open');
+      if (this.aboutSegmentedControl !== null) {
+        this.aboutSegmentedControl.addEventListener('click', (e) => {
+          this.aboutStory.classList.toggle('about-story--not-active');
+          this.aboutMission.classList.toggle('about-mission--active');
+          this.aboutSegmentedControlSelected.classList.toggle('about__segmented-control__selected--mission');
+          this.aboutSegmentedControlStory.classList.toggle('about__segmented-control__story--about-not-active');
+          this.aboutSegmentedControlMission.classList.toggle('about__segmented-control__mission--about-active');
+          
         });
       };
     },
