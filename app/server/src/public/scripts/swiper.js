cd97beada14a295de6006d28bd0e67a642d185ce
Jamie-Lee Hart <71268169+pgm-jamihart@users.noreply.github.com>
2021-06-01 23:07:49 +0200

    finished homepage

--- app/server/src/public/scripts/swiper.js
+++ app/server/src/public/scripts/swiper.js
@@ -1,19 +1,32 @@
 
 const swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 2,
-    slideShadows: true,
-  },
-  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    430: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1280: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
