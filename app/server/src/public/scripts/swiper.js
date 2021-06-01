0a653dd2639bd7e0d97ba4b63f9c03000d2ce408
Jamie-Lee Hart <71268169+pgm-jamihart@users.noreply.github.com>
2021-06-01 21:25:45 +0200

    added explore

---
+++ app/server/src/public/scripts/swiper.js
@@ -0,0 +1,22 @@
+
+const swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+
+
+
